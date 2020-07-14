5ba96ba046911b9ae4d0d1a9fec7e7ea3ceabd99
ijmondal <indrojyoti.m15@iiits.in>
2020-07-15 01:03:38 +0530

    fractional knapsack

--- 1-Algorithmic_toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ 1-Algorithmic_toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -4,9 +4,21 @@ import sys
 def get_optimal_value(capacity, weights, values):
     value = 0.
     # write your code here
-
+    fraction = [float(v)/ float(w) for v,w in zip(values, weights)]
+    for i in range(n+1):
+        if capacity == 0:
+            return value
+            break
+        max_weight = max(fraction)
+        index = fraction.index(max_weight)
+        fraction[index] = -1
+        add_capacity = min(capacity, weights[index])
+        value += add_capacity * max_weight
+        capacity -= add_capacity
+        weights[index] -= add_capacity
     return value
-
+        
+    
 
 if __name__ == "__main__":
     data = list(map(int, sys.stdin.read().split()))
