3683d9a55d0bc1d386de204476166ab1fd6e6633
ijmondal <indrojyoti.m15@iiits.in>
2020-07-14 20:40:54 +0530

    dot_product

--- 1-Algorithmic_toolbox/week3_greedy_algorithms/1_money_change/change.py
+++ 1-Algorithmic_toolbox/week3_greedy_algorithms/1_money_change/change.py
@@ -3,7 +3,16 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    coins =0
+    if m >= 10:
+        coins = m // 10
+        m = m % 10
+    if m >= 5 and m < 10:
+        coins = coins +  (m // 5)
+        m %= 5
+    if m >0 and m <5:
+        coins += m // 1
+    return coins
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
--- 1-Algorithmic_toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ 1-Algorithmic_toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -2,14 +2,31 @@
 import sys
 
 
-def compute_min_refills(distance, tank, stops):
+def compute_min_refills(distance, tank, n, stops):
     # write your code here
-    travelled = 0
-    for i,stop in enumerate(stops):
-       print(i,stop)
+    stops.insert(0,0)
+    stops.insert(n+1, distance)
+    dist_diff = [0] * len(stops)
+    refill =0
+    actual_tank = tank
+    for i in range(1, len(stops)):
+        dist_diff[i] = stops[i] - stops[i-1]
+    
+    for i in range(0, len(dist_diff)-1):
+       if dist_diff[i+1] <= tank and dist_diff[i+1] <= actual_tank:
+           tank = tank - dist_diff[i+1]
+       elif dist_diff[i+1] > actual_tank:
+           return -1
+       else:
+           tank = actual_tank - dist_diff[i+1]
+           refill +=1
+           
+           
+           
+    return refill       
         
 
 if __name__ == '__main__':
     # to stop entering multi line input, press (ctrl + d)
-    d, m, _, *stops = map(int, sys.stdin.read().split())
-    print(compute_min_refills(d, m, stops))
+    d, m, n, *stops = map(int, sys.stdin.read().split())
+    print(compute_min_refills(d, m, n, stops))
