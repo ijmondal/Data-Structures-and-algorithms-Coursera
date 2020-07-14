aa6be35ed3aad714a5b93ed207e7567dc972e440
ijmondal <indrojyoti.m15@iiits.in>
2020-07-14 23:33:08 +0530

    largest number

--- 1-Algorithmic_toolbox/week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ 1-Algorithmic_toolbox/week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -5,10 +5,25 @@ import sys
 def largest_number(a):
     #write your code here
     res = ""
-    for x in a:
-        res += x
+   
+    while len(a) != 0:
+        Max = ""
+        for x in a:
+            if isGreaterOrEqual(x,Max):
+                Max = x
+        res += Max
+        if Max in a:
+            a.remove(Max)
     return res
 
+def isGreaterOrEqual(a,b):
+    if len(b)==0:
+        return True
+    if int(a+b) >= int(b+a):
+        return True
+    else:
+        return False
+    
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = input.split()
