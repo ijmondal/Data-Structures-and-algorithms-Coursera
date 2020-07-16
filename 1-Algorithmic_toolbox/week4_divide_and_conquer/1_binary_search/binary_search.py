313af8170f544e8561f12f378fa647948bf3afad
ijmondal <indrojyoti.m15@iiits.in>
2020-07-16 23:04:59 +0530

    binary search

--- 1-Algorithmic_toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ 1-Algorithmic_toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -2,8 +2,20 @@
 import sys
 
 def binary_search(a, x):
-    left, right = 0, len(a)
+    left, right = 0, len(a)-1
     # write your code here
+    
+    while left <= right:
+        middle = (left + right ) // 2
+        if x > a[middle]:
+            left = middle +1 
+        elif x < a[middle]:
+            right = middle-1
+        else:
+            return middle
+    return -1
+
+        
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -19,4 +31,4 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
