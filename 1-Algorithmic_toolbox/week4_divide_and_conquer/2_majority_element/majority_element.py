fd0d778248fc67cff0cea48329b1449f3b6e856c
ijmondal <indrojyoti.m15@iiits.in>
2020-07-16 23:43:00 +0530

    majority element

--- 1-Algorithmic_toolbox/week4_divide_and_conquer/2_majority_element/majority_element.py
+++ 1-Algorithmic_toolbox/week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -1,18 +1,32 @@
 # Uses python3
 import sys
 
-def get_majority_element(a, left, right):
-    if left == right:
+# def get_majority_element(a, left, right):
+#     if left == right:
+#         return -1
+#     if left + 1 == right:
+#         return a[left]
+#     #write your code here
+#     return -1
+
+def get_majority_element(a,n):
+    d={}
+    for i in range(n):
+        if a[i] not in d:
+            d[a[i]]=1
+        elif a[i] in d:
+            d[a[i]] += 1
+    # print(d)
+    keyMax = max(d, key=d.get)
+    if d[keyMax] > n // 2:
+        return 1
+    else:
         return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    if get_majority_element(a, n) != -1:
         print(1)
     else:
         print(0)
