ae36bac0d50e3d1470fe5854091bf355abcca7a0
ijmondal <indrojyoti.m15@iiits.in>
2020-07-17 12:05:23 +0530

    adding week-1 solutions

--- 1-Algorithmic_toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ 1-Algorithmic_toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -2,17 +2,15 @@
 
 
 def max_pairwise_product(numbers):
-    n = len(numbers)
-    max_product = 0
-    for first in range(n):
-        for second in range(first + 1, n):
-            max_product = max(max_product,
-                numbers[first] * numbers[second])
-
-    return max_product
+    
+    a = max(numbers)
+    numbers.remove(a)
+    b = max(numbers)
+    return a*b
 
 
 if __name__ == '__main__':
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
+    # print(input_numbers)
     print(max_pairwise_product(input_numbers))
