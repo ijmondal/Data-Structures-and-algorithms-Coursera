bee20805cacba3806fe377345162b4349b6c092f
ijmondal <indrojyoti.m15@iiits.in>
2020-07-19 22:28:32 +0530

    quick sort

--- 1-Algorithmic_toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.py
+++ 1-Algorithmic_toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.py
@@ -2,9 +2,53 @@
 import sys
 import random
 
-def partition3(a, l, r):
-    #write your code here
-    pass
+def partition3(A, l, r):
+
+    lt = l  # We initiate lt to be the part that is less than the pivot
+    i = l   # We scan the array from left to right
+    gt = r  # The part that is greater than the pivot
+    pivot = A[l]    # The pivot, chosen to be the first element of the array, that why we'll randomize the first elements position
+                    # in the quick_sort function.
+    while i <= gt:      # Starting from the first element.
+        if A[i] < pivot:
+            A[lt], A[i] = A[i], A[lt]
+            lt += 1
+            i += 1
+        elif A[i] > pivot:
+            A[i], A[gt] = A[gt], A[i]
+            gt -= 1
+        else:
+            i += 1
+            
+    return lt, gt
+
+
+
+def quick_sort(A, l, r):
+    """
+    quick_sort: One of the most used sorting algorithm. 
+    It makes to recursive calls. One to sort the left part separately, other for sorting the right part.
+    The partition key is chosen randomly via ``random.randint(l, r)`` and it's between the ``l,  r``.
+    
+    PARAMETERS:
+    -----------
+    A: Array or the sequence that we want to sort.
+    l: The lower bound of the array that we want to sort. It's not very important we might replace it by a wrapper function
+    that only takes in an array as input. In this case it's the first element in the left part of the array.
+    r: It's the same as l, only differs as it's the first element from the end.
+    
+    RETURNS:
+    -------
+    Sorted list A.
+    """
+    if l >= r:
+        return
+    k = random.randint(l, r)
+    A[k], A[l] = A[l], A[k]
+    
+    lt, gt = partition3(A, l, r)
+    quick_sort(A, l, lt - 1)
+    quick_sort(A, gt + 1, r)  
 
 def partition2(a, l, r):
     x = a[l]
@@ -31,6 +75,6 @@ def randomized_quick_sort(a, l, r):
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    randomized_quick_sort(a, 0, n - 1)
+    quick_sort(a, 0, n - 1)
     for x in a:
         print(x, end=' ')
