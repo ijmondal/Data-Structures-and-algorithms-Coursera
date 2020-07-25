0bf3667e5164ee6fa4729462c0ba13a3753070ba
ijmondal <indrojyoti.m15@iiits.in>
2020-07-26 00:34:56 +0530

    inversions_update

--- 1-Algorithmic_toolbox/week4_divide_and_conquer/4_number_of_inversions/inversions.py
+++ 1-Algorithmic_toolbox/week4_divide_and_conquer/4_number_of_inversions/inversions.py
@@ -3,7 +3,7 @@ import sys
 
 def get_number_of_inversions(a, left, right):
     number_of_inversions = 0
-    if left < right:
+    if right - left >=1 :
         ave = left + (right - left) // 2
         number_of_inversions += get_number_of_inversions(a, left, ave)
         number_of_inversions += get_number_of_inversions(a, ave+1, right)
@@ -15,8 +15,8 @@ def merge(A, l, m, r):
     k = l
     n1 = m-l+1
     n2 = r-m
-    L = [0]*(n1)
-    R = [0]*(n2)
+    L = [0]*n1
+    R = [0]*n2
     
     for i in range(0 , n1): 
         L[i] = A[l + i] 
@@ -30,12 +30,18 @@ def merge(A, l, m, r):
     while i<n1 and j<n2:
         if L[i] <= R[j]:
             A[k] = L[i]
-            i += 1
-                       
+            i += 1          
         else:
             A[k] = R[j]
             j += 1
             inv += n1-i
+            # print(f"{l}, {m}, {r}")
+            # print(f"{i}, {n1}")
+            # print(inv)
+            # print(L)
+            # print(R)
+            # print(A)
+            # print("---------------------------")
         k += 1
     
     while i <n1:
@@ -50,12 +56,12 @@ def merge(A, l, m, r):
         
     return inv
 if __name__ == '__main__':
-    # input = sys.stdin.read()
-    # n, *a = list(map(int, input.split()))
-    n = 7
-    a = [6, 9, 8, 7, 3, 2, 1]
+    input = sys.stdin.read()
+    n, *a = list(map(int, input.split()))
+    # n = 7
+    # a = [6, 9, 8, 7, 3, 2, 1]
     
     
    
     print(get_number_of_inversions(a, 0, n-1))
-    print(a)
+    # print(a)
