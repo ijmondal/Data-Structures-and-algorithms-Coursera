9e9bcea7567acc9e6c22a1626996ea7df1422baa
ijmondal <indrojyoti.m15@iiits.in>
2020-08-05 11:24:55 +0530

    coin change

--- 1-Algorithmic_toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ 1-Algorithmic_toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -3,7 +3,15 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m // 4
+    coins = [1,3,4]
+    ways = [0] + ([m+1]*m)
+    
+    for i in range(1, m+1):
+        for j in coins:
+            if i >=j:
+                ways[i] = min(ways[i], 1+ways[i-j])
+    
+    return ways[m]
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
