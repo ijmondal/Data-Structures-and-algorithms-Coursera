d0f0bcb1d8aeec4f4485a419ad08b0ed444285f5
ijmondal <indrojyoti.m15@iiits.in>
2020-08-12 00:18:19 +0530

    lcs

--- 1-Algorithmic_toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ 1-Algorithmic_toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -4,7 +4,20 @@ import sys
 
 def lcs2(a, b):
     #write your code here
-    return min(len(a), len(b))
+    len_a = len(a)
+    len_b = len(b)
+    dp = [[0 for x in range(len_b + 1)] for x in range(len_a + 1)]
+    for i in range(len_a+1):
+        for j in range(len_b+1):
+            if i==0 or j==0:
+                dp[i][j] = 0
+            elif a[i-1] == b[j-1]: 
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i][j-1],        
+                                   dp[i-1][j],       
+                                   dp[i-1][j-1])
+    return dp[len_a][len_b]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
