78eccfb17aaef4f785110dfb3766d0b0d30cc29f
ijmondal <indrojyoti.m15@iiits.in>
2020-08-16 23:35:52 +0530

    knapsack

--- 1-Algorithmic_toolbox/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ 1-Algorithmic_toolbox/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -2,8 +2,32 @@
 
 import sys
 
-def lcs3(a, b, c):
+def lcs3(X, Y, Z):
     #write your code here
+    m = len(X)
+    n = len(Y)
+    o = len(Z)
+    L = [[[0 for i in range(o+1)] for j in range(n+1)] for k in range(m+1)] 
+    for i in range(m+1): 
+        for j in range(n+1): 
+            for k in range(o+1): 
+                if (i == 0 or j == 0 or k == 0): 
+                    L[i][j][k] = 0
+                      
+                elif (X[i-1] == Y[j-1] and
+                      X[i-1] == Z[k-1]): 
+                    L[i][j][k] = L[i-1][j-1][k-1] + 1
+  
+                else: 
+                    L[i][j][k] = max(max(L[i-1][j][k], 
+                                        L[i][j-1][k]
+                                        ), 
+                                    L[i][j][k-1]
+                                    ) 
+  
+    # L[m][n][o] contains length of LCS for 
+    # X[0..n-1] and Y[0..m-1] and Z[0..o-1] 
+    return L[m][n][o] 
     return min(len(a), len(b), len(c))
 
 if __name__ == '__main__':
--- 1-Algorithmic_toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ 1-Algorithmic_toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,15 +1,24 @@
 # Uses python3
 import sys
 
-def optimal_weight(W, w):
+def optimal_weight(W, wt):
     # write your code here
     result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    dp = [[0 for x in range(W + 1)] for x in range(len(wt) + 1)] 
+    for i in range(len(wt)+1):
+        for w in range(W+1):
+            if i == 0 or w == 0: 
+                dp[i][w] = 0
+            elif wt[i-1] <= w:
+                dp[i][w] = max(dp[i-1][w], wt[i-1]+dp[i-1][w-wt[i-1]])
+            else:
+                dp[i][w] = dp[i-1][w]
+        
+    
+    # print(dp)       
+    return dp[len(wt)][W]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     W, n, *w = list(map(int, input.split()))
-    print(optimal_weight(W, w))
+    print(optimal_weight(W,w))
