079b5f70d5928811e767d1a9bcbb2c14b2a6bb88
ijmondal <indrojyoti.m15@iiits.in>
2020-08-17 13:18:19 +0530

    partitioning into k equal sum subsets

--- 1-Algorithmic_toolbox/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ 1-Algorithmic_toolbox/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -2,19 +2,41 @@
 import sys
 import itertools
 
-def partition3(A):
-    for c in itertools.product(range(3), repeat=len(A)):
-        sums = [None] * 3
-        for i in range(3):
-            sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
+def canPartition3(A):
+    s = sum(A)
+    target, rem = divmod(s,3)
+    
+    if rem !=0 or max(A) > target:
+        return False
+    
+    nums = sorted(A, reverse=True)
+    return search(nums, 3, target, target, set())
 
-        if sums[0] == sums[1] and sums[1] == sums[2]:
-            return 1
-
-    return 0
+def search(nums, k, cur_target, full_target, visited):
+    if k == 0:
+        return True        
+        
+    elif cur_target < 0:
+        return False
+        
+    elif cur_target == 0:
+        return search(nums, k-1, full_target, full_target, visited)
+        
+    else:            
+        for i in range(0, len(nums)):
+            if i not in visited:
+                visited.add(i)
+                if search(nums, k, cur_target-nums[i], full_target, visited):           
+                    return True
+                visited.remove(i)
+        return False
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *A = list(map(int, input.split()))
-    print(partition3(A))
+    if canPartition3(A):
+        print(1)
+    else:
+        print(0)
+    
 
