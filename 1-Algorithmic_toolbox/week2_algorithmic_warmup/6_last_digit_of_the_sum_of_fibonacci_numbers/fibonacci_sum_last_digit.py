7f0a127daeead4fcf73d4b710b9e0482fa19f715
ijmondal <indrojyoti.m15@iiits.in>
2020-08-17 13:35:46 +0530

    algithmic warm-up

--- 1-Algorithmic_toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ 1-Algorithmic_toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -1,21 +1,25 @@
-# Uses python3
-import sys
+n = int(input())
 
-def fibonacci_sum_naive(n):
-    if n <= 1:
-        return n
+if n<=1:
+    print(n)  
+    quit()
 
-    previous = 0
-    current  = 1
-    sum      = 1
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum += current
-
-    return sum % 10
-
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(fibonacci_sum_naive(n))
+lesser = (n+2)%60
+if lesser==1:
+    print(0)
+    quit()
+elif lesser==0:
+    print(9)
+    quit()
+def fibo(n):
+    a,b = 0, 1
+    for _ in range(2,lesser+1):
+        c = a+b
+        c = c%10
+        b, a = c, b
+    if c!=0:
+        print(c-1)
+    else:
+        print(9)
+fibo(lesser)
